dd6968e5d15865f5cd04bfe266260062d0f00c74
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2014-09-13 00:39:26 +0200

    Remove test file

--- test.py
+++ test.py
@@ -7,6 +7,6 @@ c.sasl_interactive_bind_s("", ldap.sasl.external())
 
 import sys
 
-for dn, att in c.search_s('o=formiris', ldap.SCOPE_SUBTREE, attrlist=['cn'],
-        offset=int(sys.argv[1]), length=int(sys.argv[2]), ordering='ou:1.3.6.1.4.1.1466.109.114.2'):
+for dn, att in c.search_s('ou=interne,ou=personnes,o=formiris', ldap.SCOPE_SUBTREE, 'objectClass=person', attrlist=['cn'],
+        offset=int(sys.argv[1]), length=int(sys.argv[2]), ordering='cn:1.3.6.1.4.1.1466.109.114.2'):
     print dn
